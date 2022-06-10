0deed6a2d3940620d9196892512703164c32f90d
xovest <79008917+xovest@users.noreply.github.com>
2022-06-10 08:25:18 +0300

    yet another one

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='appTop'>
+
+      </div>
+
+      <div className='appVideos'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
